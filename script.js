f8b2422b8ee124970115e358456cde84cbbce7b4
Shahid <109452231+raza-shahi@users.noreply.github.com>
2025-12-31 12:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const makeChange = (c) => {
   // your name here
+	amount = c;
+	let obj = { 0: ["q", 0.25], 1: ["d", 0.10], 2: ["n", 0.05], 3: ["p", 0.01] };
 	let res = {};
 
 for (let i =0;i<4;i++){
-  let [coin, value] = c[i];
+  let [coin, value] = obj[i];
   value = value * 100;
   let count = Math.floor(amount / value);
   if (count >= 0){
